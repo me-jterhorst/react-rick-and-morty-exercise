aac76078c4670f740a4ba21c817f3dc6becaa7d1
Jakob <me@jterhorst.de>
2021-07-23 15:14:53 +0200

    Add Links around Character cards

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,13 +1,16 @@
 import "./Charactes.css";
 import Form from "./Form";
 import CharacterCard from "./CharacterCard";
+import { Link } from "react-router-dom";
 
 export default function Characters() {
   return (
     <section className="characters">
       <Form />
       <ul className="character-list">
-        <CharacterCard />
+        <Link to="/characters/singleCharacter">
+          <CharacterCard />
+        </Link>
       </ul>
       <button>More</button>
     </section>
